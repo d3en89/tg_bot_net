c9bf1f581e71eb67dfe6cb231aed5e18e8c0d70f
d3en89 <stdenis889@yandex.ru>
2024-01-02 14:30:46 +0300

    Переименовал nslookup

--- bot_utils/__init__.py
+++ bot_utils/__init__.py
@@ -1,5 +1,5 @@
 from bot_utils import gen
 from bot_utils import get_speedtest
 from bot_utils.get_zabbix import zabbix_get
-from bot_utils import nslook
+from bot_utils import my_nslook
 from bot_utils import port_ping
