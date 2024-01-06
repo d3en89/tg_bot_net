778f54ae58ca0079b75037f8a62dcb53349de98f
d3en89 <stdenis889@yandex.ru>
2024-01-06 20:43:59 +0300

    Заменил декоратор проверки доступа на midleware

---
+++ handlers/my_midlewares.py
@@ -0,0 +1,21 @@
+from aiogram import types
+from aiogram.dispatcher.middlewares import BaseMiddleware
+
+from bot import dp
+from read_config import watch_id
+
+
+class CheckAccess(BaseMiddleware):
+    """ Класс для проверки разрешения на использование бота
+    """
+    def __init__(self):
+        BaseMiddleware.__init__(self)
+
+    async def on_pre_process_message(self, message: types.Message, data: dict):
+        if message.text == '/id':
+            return data
+        elif watch_id(message.from_user.id):
+            return data
+        else:
+           await dp.bot.send_message(message.chat.id,'Вас нет в списке разрешенных')
+           raise PermissionError('Вас нет в списке разрешенных')
